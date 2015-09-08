f83f171033616565b1fec0adce9ff105ab881bd7
gbrlmrllo <gbrlmrllo@gbrlmrllo-desktop.(none)>
2015-09-08 09:48:17 -0430

    sd

--- app/modules/app-user/app-user-index.js
+++ app/modules/app-user/app-user-index.js
@@ -3,29 +3,24 @@
     is: 'app-user-index',
     propierties: {
       name: String,
-      lastname: String
+      lastname: String,
+      users: Array
     },
+
     ready: function(){      
-      this.name = "Nombre...";
-      this.lastname = "Apellido...";
-        this.users = [
-            { name: "Rob", lastname: "Tompson" },
-            { name: "Maria", lastname: "James" },
-            { name: "Carl", lastname: "Stevens" },
-            { name: "Ken", lastname: "Robinson" }
-        ]
+
     },
     
     clickBtn: function(event){  
-        person = event.model.item;
-        this.name = person.name;
-        this.lastname = person.lastname;
+      person = event.model.item;
+      this.name = person.name;
+      this.lastname = person.lastname;
     },
 
     clickBtnTwo: function(event){
-        person = event.toElement.attributes;        
-        this.name = person.name.value;
-        this.lastname = person.lastname.value;
+      person = event.toElement.attributes;        
+      this.name = person.name.value;
+      this.lastname = person.lastname.value;
     }
   });
 })();
